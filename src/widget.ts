0eed6a457ae427fd334dafc9d87c308cfa34d534
kakuiho <61496907+kakuiho@users.noreply.github.com>
2024-12-11 11:09:50 +0800

    Added function newWidget.

--- src/widget.ts
+++ src/widget.ts
@@ -1,3 +1,5 @@
+import styles from "./widget.module.css";
+
 export interface Work {
   cn_url?: string;
   cn_rating?: string;
@@ -10,74 +12,35 @@ export interface DataSourceRating {
   url: string;
 }
 
-export const cssNsPrefix = "filmbuddLite";
-
 type FuncAttach = (doc: Document, root: Node, node: Node) => void;
 
-export function attachWidget(
-  doc: Document,
-  rootNode: Node,
-  workInfo: Work,
-  classes: CSSModule,
-  funcAttach?: FuncAttach
-) {
-  const defaultClassName = cssNsPrefix + "Widget";
-  const clsWidget = [defaultClassName, classes[cssNsPrefix + "Widget"]!];
-  const widget = doc.createElement("div");
-  widget.classList.add(...clsWidget);
-
-  const { cn_url, cn_rating } = workInfo;
-  const ratingCellCN = newRatingCell(
-    doc,
-    "cn",
-    {
-      rating: cn_rating!,
-      url: cn_url!,
-    },
-    classes
-  );
-
-  const { im_url, im_rating } = workInfo;
-  const ratingCellIM = newRatingCell(
-    doc,
-    "im",
-    {
-      rating: im_rating!,
-      url: im_url!,
-    },
-    classes
-  );
-
-  cn_rating && cn_rating !== "" && widget.appendChild(ratingCellCN);
-  im_rating && im_rating !== "" && widget.appendChild(ratingCellIM);
-
+export function attachWidget(doc: Document, rootNode: Node, workInfo: Work, _classes: typeof styles, funcAttach?: FuncAttach) {
+  const widget = newWidget(doc, workInfo, _classes);
   funcAttach ? funcAttach(doc, rootNode, widget) : rootNode.appendChild(widget);
 }
 
-function newRatingCell(
-  doc: Document,
-  dataSource: string,
-  ratingInfo: DataSourceRating,
-  classes: any
-) {
+export function newRatingCell(doc: Document, dataSource: string, ratingInfo: DataSourceRating, _classes: typeof styles) {
   const cell = doc.createElement("a");
   cell.href = ratingInfo.url;
   cell.target = "_blank";
-  const clsCell = [classes["wrapper"]];
-  cell.classList.add(...clsCell);
+  cell.classList.add(_classes.wrapper);
 
-  const clsWrapper = [classes["ratingCell"], classes[`${dataSource}`]];
   const wrapper = doc.createElement("span");
-  wrapper.classList.add(...clsWrapper);
 
-  const clsRating = [classes["cellChunk"], classes["rating"]];
+  const _m = _classes as { [key: string]: string };
+  const classDateSource = _m[`${dataSource}`] as string;
+
+  const classesWrapper = [_classes.ratingCell, classDateSource];
+  wrapper.classList.add(...classesWrapper);
+
   const rating = doc.createElement("span");
-  rating.classList.add(...clsRating);
+  const classesRating = [_classes.cellChunk, _classes.rating];
+  rating.classList.add(...classesRating);
   rating.textContent = ratingInfo.rating;
 
-  const clsIcon = [classes["cellChunk"], classes["icon"]];
   const icon = doc.createElement("span");
-  icon.classList.add(...clsIcon);
+  const classesIcon = [_classes.cellChunk, _classes.icon];
+  icon.classList.add(...classesIcon);
   icon.textContent = dataSource.toUpperCase();
 
   wrapper.appendChild(rating);
@@ -89,3 +52,35 @@ function newRatingCell(
 
 export const MidLineEllipsis = "⋯";
 export const ErrorFace = "x_x*";
+
+export function newWidget(doc: Document, workInfo: Work, _classes: typeof styles) {
+  const widget = doc.createElement("div");
+  widget.classList.add(_classes.filmbuddLiteWidget);
+
+  const { cn_url, cn_rating } = workInfo;
+  const ratingCellCN = newRatingCell(
+    doc,
+    "cn",
+    {
+      rating: cn_rating!,
+      url: cn_url!,
+    },
+    _classes
+  );
+
+  const { im_url, im_rating } = workInfo;
+  const ratingCellIM = newRatingCell(
+    doc,
+    "im",
+    {
+      rating: im_rating!,
+      url: im_url!,
+    },
+    _classes
+  );
+
+  cn_rating && cn_rating !== "" && widget.appendChild(ratingCellCN);
+  im_rating && im_rating !== "" && widget.appendChild(ratingCellIM);
+
+  return widget;
+}
